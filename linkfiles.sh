86080ca67db2bad6d45b69320b2d3e23f3a98306
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2015-09-16 16:27:13 -0300

    Script para criar os soft links para os diretórios
    Se o usuário estiver usando Linux, facilita a vida dele com esse
    script. Vai facilitar também no caso de querer mandar patches.

---
+++ linkfiles.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+#
+# Cria links simbolicos nos lugares apropriados
+#
+
+ln -s $(pwd)/texmf/bibtex/bst/ThesisPUC ~/texmf/bibtex/bst/ThesisPUC
+ln -s $(pwd)/texmf/tex/latex/ThesisPUC ~/texmf/tex/latex/ThesisPUC
+
