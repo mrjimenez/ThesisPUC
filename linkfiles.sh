032c60e4eea4e73619e9a47d7b3a4844a28ed49a
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2015-10-01 18:42:33 -0300

    Melhora o script

--- linkfiles.sh
+++ linkfiles.sh
@@ -3,6 +3,8 @@
 # Cria links simbolicos nos lugares apropriados
 #
 
+mkdir -p  ~/texmf/bibtex/bst/
 ln -s $(pwd)/texmf/bibtex/bst/ThesisPUC ~/texmf/bibtex/bst/ThesisPUC
+mkdir -p ~/texmf/tex/latex/
 ln -s $(pwd)/texmf/tex/latex/ThesisPUC ~/texmf/tex/latex/ThesisPUC
 
